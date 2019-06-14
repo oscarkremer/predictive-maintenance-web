2d2261c595bee72eb8128d71b2b79f40a16fa507
oscarkremer <oscar.s.kremer@hotmail.com>
2019-06-13 21:00:35 -0300

    first commit: cookie cutter data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predictive maintenance algorithms',
+    author='Oscar Schmitt Kremer',
+    license='MIT',
+)
